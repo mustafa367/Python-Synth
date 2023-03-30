cd12e04f0f034b0c9de82b99f87d88228d01c9bc
mustafa367 <57695339+mustafa367@users.noreply.github.com>
2023-03-29 21:20:13 -0400

    Shepard tone attempt

--- app/main.py
+++ app/main.py
@@ -8,12 +8,16 @@ def sawtooth(freq,amp):return lambda x:(((x%(1/freq))*freq)-.5)*2*amp
 def square(freq,amp):return lambda x:amp if (sawtooth(freq,amp)(x)>0) else -1*amp
 def triangle(freq,amp):return lambda x:2*(sawtooth(freq,amp)(x)*square(freq,amp)(x)-amp/2)
 
+def shepard_tone_a(freq,amp,x):
+    f=lambda z,i:(0 if z+i==0 else amp/(z+i)*np.sin(freq*2*np.pi*((z+i)**2)))
+    return sum([f(x,i) for i in range(64)])
+
 if __name__ == "__main__":
     out_dir='../out'
     now=datetime.datetime.now()
     now_str=now.strftime("%Y:%m:%d:%H:%M:%S")
     out_name=out_dir+"/test_"+now_str+".wav"
 
-    f=triangle(440,1)
-    sq=audio_out(f)
-    sq.wav_out(out_name)
+    f=lambda x:shepard_tone_a(440,1,x)
+
+    audio_out(f).wav_out(out_name)
