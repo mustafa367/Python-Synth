b113149988b1cfcd299b6ebe95fbd8b0c9bd8c62
mustafa367 <57695339+mustafa367@users.noreply.github.com>
2023-04-01 10:38:27 -0400

    Added second shepard tone attempt

--- app/main.py
+++ app/main.py
@@ -12,12 +12,17 @@ def shepard_tone_a(freq,amp,x):
     f=lambda z,i:(0 if z+i==0 else amp/(z+i)*np.sin(freq*2*np.pi*((z+i)**2)))
     return sum([f(x,i) for i in range(64)])
 
+def shepard_tone_b(freq,amp,x):
+    f=lambda z:amp/(z**2+1)*np.sin(freq*2*np.pi*(2**z))
+    g=lambda z,i:f(z-i)
+    return sum([g(x,i) for i in range(64)])
+
 if __name__ == "__main__":
     out_dir='../out'
     now=datetime.datetime.now()
     now_str=now.strftime("%Y:%m:%d:%H:%M:%S")
     out_name=out_dir+"/test_"+now_str+".wav"
 
-    f=lambda x:shepard_tone_a(440,1,x)
+    f=lambda x:shepard_tone_b(440,1,x)
 
     audio_out(f).wav_out(out_name)
