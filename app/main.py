dbcc03157c904514c8b4ffbd825e6c3c6fdd27a5
mustafa367 <57695339+mustafa367@users.noreply.github.com>
2023-05-17 07:01:35 -0400

    Simplified tone class

--- app/main.py
+++ app/main.py
@@ -9,7 +9,10 @@ def square(freq,amp):return lambda x:amp if (sawtooth(freq,amp)(x)>0) else -1*am
 def triangle(freq,amp):return lambda x:2*(sawtooth(freq,amp)(x)*square(freq,amp)(x)-amp/2)
 
 if __name__ == "__main__":
-    f=sin(440,1)
-    t=tone(f,t0=1)
-    f=t.get_f()
-    audio_out(f)
+    f=sin(330,1)
+    tone1=tone(f)
+    g=sin(220,1)
+    tone2=tone(g)
+    tone3=tone1+tone2
+
+    audio_out(tone3.get)
--- app/tone.py
+++ app/tone.py
@@ -1,26 +1,26 @@
 import numpy as np
 
 class tone:
-    def __init__(self,f,volume=1,t0=0,sustain=100,attack=0,release=0):
-        t1=t0+attack
-        t2=t1+sustain
-        t3=t2+release
-        def g(t):
-            if t<t0:
-                return 0
-            elif t<t1:
-                return f(t)*volume*(t-t0)
-            elif t<t2:
-                return f(t)*volume
-            elif t<t3:
-                return f(t)*volume*(t3-t)/release
-            else:
-                return 0
-        self.f=g
+    def __init__(self,f,volume=1,t0=0,sustain=20,attack=0,release=0):
+        self.volume=volume
+        self.f=f
+        self.t0=t0
+        self.t1=self.t0+attack
+        self.t2=self.t1+sustain
+        self.t3=self.t2+release
 
-    def get_f(self):
-        return self.f
+    def get(self,t):
+        if t<self.t0:
+            return 0
+        elif t<self.t1:
+            return self.f(t)*self.volume*(t-self.t0)
+        elif t<self.t2:
+            return self.f(t)*self.volume
+        elif t<self.t3:
+            return self.f(t)*self.volume*(self.t3-t)/release
+        else:
+            return 0
 
     def __add__(self,othertone):
-        f=lambda t:self.get_f(t)+othertone.get_f(t)
+        f=lambda t:self.get(t)+othertone.get(t)
         return tone(f)
