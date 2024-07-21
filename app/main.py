907880bfe125498bd5e788e7e3e047b4d65dbdeb
mustafa367 <57695339+mustafa367@users.noreply.github.com>
2024-07-21 02:48:36 +0000

    Progressing to shepard tone generation

--- app/main.py
+++ app/main.py
@@ -1,21 +1,39 @@
 import numpy as np
-from scipy.io import wavfile
 import datetime
+from matplotlib import pyplot
+from scipy.io import wavfile
+
 from tone import tone
+from audio_out import audio_out
 
 def main():
-    f=sin(330,1)
-    tone1=tone(f)
-    g=sin(220,1)
-    tone2=tone(g)
-    tone3=tone1+tone2
+    f0 = .5
+    f1 = 2
+    duration = 5
 
-    wav_write(tone3.get)
+    bound = lambda x: min(max(x, 0), duration)
+    f = lambda x: f0 * 2 ** (np.log2(f1 / f0) * (bound(x) / duration))
+    A = lambda x: -1 * np.abs(2 * (bound(x) - duration / 2) / duration) + 1
+    X = np.arange(0, 5 , 1/44100)
+
+    ## Plots to verify shape of freq and amp
+    # pyplot.plot(X, [f(x) for x in X])
+    # pyplot.savefig("./out/shepard_test_f.png")
+    # pyplot.close()
+    # pyplot.plot(X, [A(x) for x in X])
+    # pyplot.savefig("./out/shepard_test_A.png")
+
+    base_form = lambda x: A(x)*np.sin(f(x)*x*2*np.pi)
+    Y = np.array([base_form(x) for x in X])
+    pyplot.plot(X, Y)
+    pyplot.savefig("./out/shepard_test_base_form.png")
+    wav_write(Y)
+
     return
 
-def wav_write(LPCM ,sample_rate=44100,volume=.1,
-                 filename="../out"+"/test_"+datetime.datetime.now().strftime("%Y:%m:%d:%H:%M:%S")+".wav"):
-    wavfile.write(filename,sample_rate, volume * LPCM)
+def wav_write(LPCM ,sample_rate=44100,
+                 filename="./out"+"/test_"+datetime.datetime.now().strftime("%Y:%m:%d:%H:%M:%S")+".wav"):
+    wavfile.write(filename,sample_rate, LPCM)
     return
 
 #Wave generators
