0a93f0d3bf84d266ab2e711b2cd1f16f0cc56b71
mustafa367 <57695339+mustafa367@users.noreply.github.com>
2024-07-21 03:26:15 +0000

    Shepard Output and Offsett-Adding Base Form

--- app/main.py
+++ app/main.py
@@ -7,14 +7,19 @@ from tone import tone
 from audio_out import audio_out
 
 def main():
-    f0 = .5
-    f1 = 2
-    duration = 5
+    f0 = 220
+    f1 = 880
+    duration = 12
 
     bound = lambda x: min(max(x, 0), duration)
     f = lambda x: f0 * 2 ** (np.log2(f1 / f0) * (bound(x) / duration))
+    # A = lambda x: -1 * np.abs(2 * (bound(x) - duration / 2) / duration) + 1
     A = lambda x: -1 * np.abs(2 * (bound(x) - duration / 2) / duration) + 1
-    X = np.arange(0, 5 , 1/44100)
+    
+    base_form = lambda x: A(x)*np.sin(f(x)*x*2*np.pi)
+
+    X = np.arange(0, 12 , 1/44100)
+    Y = np.array([base_form(x) for x in X]).astype(np.float32)
 
     ## Plots to verify shape of freq and amp
     # pyplot.plot(X, [f(x) for x in X])
@@ -22,18 +27,33 @@ def main():
     # pyplot.close()
     # pyplot.plot(X, [A(x) for x in X])
     # pyplot.savefig("./out/shepard_test_A.png")
+    # pyplot.plot(X, Y)
+    # pyplot.savefig("./out/shepard_test_base_form.png")
 
-    base_form = lambda x: A(x)*np.sin(f(x)*x*2*np.pi)
-    Y = np.array([base_form(x) for x in X])
-    pyplot.plot(X, Y)
-    pyplot.savefig("./out/shepard_test_base_form.png")
-    wav_write(Y)
+    n = 5
+    m = 24
+
+    # offset = int(len(Y) / 2)
+    # Z = Y + np.concat([Y[offset:],Y[:offset]])
+    # out = np.concat([Y[:offset]] + [Z] * 10  + [Y[offset:]])
+
+    offset = int(len(Y) / n)
+    out = np.empty(Y.size * m).astype(np.float32)
+    before = 0
+    after = out.size - Y.size
+    for i in range(m):
+        Y_padded = np.pad(Y, (before, after), 'constant', constant_values=(0, 0))
+        out = out + Y_padded
+        before += offset
+        after -= offset
+
+    wav_write(out)
 
     return
 
-def wav_write(LPCM ,sample_rate=44100,
+def wav_write(LPCM ,sample_rate=44100, volume = .05,
                  filename="./out"+"/test_"+datetime.datetime.now().strftime("%Y:%m:%d:%H:%M:%S")+".wav"):
-    wavfile.write(filename,sample_rate, LPCM)
+    wavfile.write(filename,sample_rate, volume * LPCM)
     return
 
 #Wave generators
